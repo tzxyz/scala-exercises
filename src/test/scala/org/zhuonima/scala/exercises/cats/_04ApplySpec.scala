92991e5021ef09324f052a202f08eb426f4effd2
zhuonima <tz.zhuo@protonmail.com>
2017-09-15 11:30:43 +0800

    add  more tests

--- src/test/scala/org/zhuonima/scala/exercises/cats/_04ApplySpec.scala
+++ src/test/scala/org/zhuonima/scala/exercises/cats/_04ApplySpec.scala
@@ -10,6 +10,8 @@ class _04ApplySpec extends FlatSpec with Matchers {
   private val intToString: Int => String = _.toString
   private val double: Int => Int = _ * 2
   private val addTwo: Int => Int = _ + 2
+  private val addArity2 = (a: Int, b: Int) => a + b
+  private val addArity3 = (a: Int, b: Int, c: Int) => a + b + c
 
   "Apply Map Spec" should "have some tests" in {
     Apply[Option].map(Some(1))(intToString) should be(Some("1"))
@@ -29,6 +31,36 @@ class _04ApplySpec extends FlatSpec with Matchers {
     Apply[Option].ap(Some(double))(None) should be(None)
     Apply[Option].ap(None)(Some(1)) should be(None)
     Apply[Option].ap(None)(None) should be(None)
+  }
+
+  "Apply Ap2, Ap3 Spec" should "have some tests" in {
+    Apply[Option].ap2(Some(addArity2))(Some(1), Some(2)) should be(Some(3))
+    Apply[Option].ap2(Some(addArity2))(Some(1), None) should be(None)
+    Apply[Option].ap3(Some(addArity3))(Some(1), Some(2), Some(3)) should be(Some(6))
+  }
+
+  "Apply Map2, Map3 Spec" should "have some tests" in {
+    Apply[Option].map2(Some(1), Some(2))(addArity2) should be(Some(3))
+    Apply[Option].map3(Some(1), Some(2), Some(3))(addArity3) should be(Some(6))
+  }
+
+  "Apply Tuple2, Tuple3 Spec" should "have some tests" in {
+    Apply[Option].tuple2(Some(1), Some(2)) should be(Some((1, 2)))
+    Apply[Option].tuple3(Some(1), Some(2), Some(3)) should be(Some(1, 2, 3))
+  }
+
+  "Apply Builder Syntax Spec" should "have some tests" in {
+    val option2 = Option(1) |@| Option(2)
+    val option3 = option2 |@| Option.empty[Int]
+
+    option2 map addArity2 should be(Some(3))
+    option3 map addArity3 should be(None)
+
+    option2 apWith Some(addArity2) should be(Some(3))
+    option3 apWith Some(addArity3) should be(None)
+
+    option2.tupled should be(Some(1, 2))
+    option3.tupled should be(None)
   }
 
 }
