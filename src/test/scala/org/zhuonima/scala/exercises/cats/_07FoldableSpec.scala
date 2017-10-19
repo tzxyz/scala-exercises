3c3fa4369a547cf4851b445c91bfcfe9266cab6c
zhuonima <tz.zhuo@protonmail.com>
2017-10-19 16:34:55 +0800

    update foldable specs

--- src/test/scala/org/zhuonima/scala/exercises/cats/_07FoldableSpec.scala
+++ src/test/scala/org/zhuonima/scala/exercises/cats/_07FoldableSpec.scala
@@ -26,4 +26,35 @@ class _07FoldableSpec extends FlatSpec with Matchers {
     Foldable[List].foldMap(List(1, 2, 3))(_.toString) should be("123")
   }
 
+  "FoldK" should "have some tests" in {
+    Foldable[List].foldK(List(List(1, 2), List(3, 4, 5))) should be(List(1, 2, 3, 4, 5))
+    Foldable[List].foldK(List(None, Option("two"), Option("three"))) should be(Some("two"))
+  }
+
+  "Find" should "have some tests" in {
+    Foldable[List].find(List(1, 2, 3))(_ > 2) should be(Some(3))
+    Foldable[List].find(List(1, 2, 3))(_ > 5) should be(None)
+  }
+
+  "Exists" should "exists checks whether at least one element satisfies the predicat" in {
+    Foldable[List].exists(List(1, 2, 3))(_ > 2) should be(true)
+    Foldable[List].exists(List(1, 2, 3))(_ > 5) should be(false)
+  }
+
+  "Forall" should "forall checks whether all elements satisfy the predicate" in {
+    Foldable[List].forall(List(1, 2, 3))(_ <= 3) should be(true)
+    Foldable[List].forall(List(1, 2, 3))(_ < 3) should be(false)
+  }
+
+  "To List" should "convert F[A] to List[A]" in {
+    Foldable[List].toList(List(1, 2, 3)) should be(List(1, 2, 3))
+    Foldable[Option].toList(Option(42)) should be(List(42))
+    Foldable[Option].toList(None) should be(List())
+  }
+
+  "Filter" should "convert F[A] to List[A] only including the elements that match a predicate" in {
+    Foldable[List].filter_(List(1, 2, 3))(_ < 3) should be(List(1, 2))
+    Foldable[Option].filter_(Option(42))(_ != 42) should be(List())
+  }
+
 }
